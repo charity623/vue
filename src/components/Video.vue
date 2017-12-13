33ad2126c6cfbc3933fd76163450c2b07b5a49ab
chenrt <491345185@qq.com>
2017-12-13 16:01:38 +0800

    5-7

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,9 +1,9 @@
 <template>
-
+<p style="color:blue">这是p标签</p>
 </template>
 
 <style scoped>
-
+p{color:red;}
 
 </style>
 
@@ -16,7 +16,9 @@ export default {
 	created() {
         // this.init();
         // this.fn();
-        this.fn1();
+        // this.fn1();
+        // this.api();
+        this.webApi();
 	},
 	mounted() {
 		
@@ -35,7 +37,7 @@ export default {
             var a={age:20};
             var b=a;
             b.age = 21;
-            console.log('init2='+a.age)
+            console.log('init2='+a.age)//21
             //typeof运算符
 
             //值类型
@@ -98,9 +100,8 @@ export default {
                 var age;
             }
         },
-        //this 要在执行时才能确认值，定义时无法确认
-
         fn1(){
+            //this 要在执行时才能确认值，定义时无法确认
             var a = {
                 'name':'zhangsan',
                 fn:function(){
@@ -192,28 +193,139 @@ export default {
                 }
             }
             var firstLoad = isFirstLoad();
-            firstLoad(10);
-            firstLoad(10);
-            firstLoad(20);
+            firstLoad(10);//true
+            firstLoad(10);//false
+            firstLoad(20);//true
 
             //异步
             //1.什么是异步
             // 2.使用场景 
             //   1、定时任务 setTimeout setInverval
             //   2、网络请求 ajax请求 动态加载<img>
-            //   3、事件绑定 click keyup...
-            //3.异步和单线程
+            //   3、事件绑定 click keyup... （和上面两个的区别：可以执行多次。相同点：都需要等待） 
+            //3.异步和单线程（）
 
             //异步场景
             console.log(100)
             setTimeout(function(){console.log(200)},1000)
             console.log(300)  //100 300 200
+            
             //对比同步
             console.log(100)
             alert(200)
             console.log(300)  //同步会有阻塞
+            
+            //单线程
+            console.log(100)
+            setTimeout(function(){
+                console.log(200)
+            }) // 遇到异步。。先拿出来。。把能执行的都执行了之后，如果能立即执行 就执行
+            console.log(300) //100 300 200
+            //理解：执行100 
+                    // 执行setTimeout后，传入setTimeout的函数会被暂存起来，不会立即执行（单线程特点：不会同时干两件事）
+                    // 执行300
+                    // 待所有程序执行完，处于空闲状态时，会立马看有没有暂存起来的要执行
+                    // 发现暂存起来的setTimeout中的函数 无需等待时间，就立即执行
+            //同步和异步区别
+            // 1、同步会阻塞代码
+            // 2、alert是同步 setTimeout是异步
+        },
+        api(){
+            //Date
+            console.log(Date.now()) // 1513142732675  获取当前时间毫秒数,从1970年开始
+            var d = new Date();
+            console.log("获取当前毫秒数："+d.getTime())// 获取毫秒数
+            console.log("年："+d.getFullYear())//年
+            console.log("月："+parseInt(d.getMonth()+1))//月 （0-11）当前月份要+1
+            console.log("日："+d.getDate())//日 （0-31）
+            console.log("时："+d.getHours())//小时 （0-23）
+            console.log("分："+d.getMinutes())//分钟（0-59）
+            console.log("秒："+d.getSeconds())//秒（0-59）
+            //Math
+            console.log(Math.random()) //主要清楚缓存用
+            var random  = Math.random();
+            var random = random + '0000000000' //后面加10个0
+            var random = random.slice(0,10);
+            console.log("获取随机数"+random)
+            //数组api
+            // forEach 遍历所有元素
+            // every 判断所有元素是否都符合条件
+            // some 判断是否有至少一个元素符合
+            // sort 排序
+            // map 对元素重新组装，生成新数组
+            // filter 过滤符合条件的元素 
+            var arr = [1,3,2,5];
+            arr.forEach(function(item,index){
+                //遍历数组的所有元素 item：值，index：下标
+                console.log(index,item)
+            })
+            var result = arr.every(function(item,index){
+                //用来判断所有的数组元素，都满足一个条件
+                if(item<4){
+                    return true;
+                }
+            })
+            console.log("数组结果："+result)//true 
 
+            var _someresult = arr.some(function(item,index){
+                //有一个满足条件即可
+                if(item<2){
+                    return true;
+                }
+            })
+            console.log("some数组结果"+_someresult);
 
+            var _sortresult = arr.sort(function(a,b){
+                //从小到大
+                return a-b;
+            })
+            console.log("sort排序："+_sortresult)
+
+            var arr2 = arr.map(function(item,index){
+                return '<b>'+(index+1)+':'+item+'</b>'
+            })
+            console.log(arr2);
+
+            //对象api
+            var obj = {
+                x:100,
+                y:200,
+                z:300
+            }
+            var key;
+            for(key in obj){
+                //hasOwnProperty:判断是否是定义的对象自身的属性，而不是继承的
+                if(obj.hasOwnProperty(key)){
+                    console.log(key,obj[key])
+                }
+            }
+        },
+        webApi(){
+            // 常说的js（浏览器执行的js）包含两部分：js基础知识（ECMA262标准）和js-web-api（w3c标准）
+            //js基础知识：1.变量类型和计算  2.原型和原型链 3.闭包和作用域 4.其他api （基于ECMA262标准）
+            //js-web-api：w3c标准（不管变量类型、原型、作用域、异步）
+            //js内置的全局函数和对象
+            //window document navigator
+            //q1：DOM是那种基本的数据结构
+            //q2：DOM操作的常用api有哪些 
+            //q3：DOM元素的attribute和property有何区别
+            var div1 = document.getElementById('div1');//元素
+            var divlist = document.getElementsByTagName('div');//集合
+            var containerlist = document.getElementsByClassName('.container')//集合
+            var plist = document.querySelectorAll('p');
+            console.log(plist[0]);
+            //DOM结构操作 
+            // 1.新增节点 
+            // 1）createElement
+            // 2）appendChild()
+            // 2.获取父元素  （递归：自身循环的过程）
+            // parentElement();
+            // 3.获取子元素 
+            // childNodes();
+            // 4.删除节点
+            // removeChild();
+
+            //该看5-7了
         }
         
 	},
